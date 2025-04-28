7b1e7992e1fcbaedb5cc32773b99fd529ba6a129
arislan <kootlook.applications@gmail.com>
2025-04-28 15:07:31 +0500

    refactor: remove excessive const in create module

--- src/fs/create.js
+++ src/fs/create.js
@@ -7,8 +7,7 @@ const create = async () => {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
 
-    const dir = path.join(__dirname, "files");
-    const filePath = path.join(dir, "fresh.txt");
+    const filePath = path.join(__dirname, "files", "fresh.txt");
 
     try {
       await fs.access(filePath);
